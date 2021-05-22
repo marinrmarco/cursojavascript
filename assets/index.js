a8311b3a77366c9994c6478330b14815874c957c
marinrmarco <marinrosalesmarco@gmail.com>
2021-05-22 15:40:48 -0300

    Scopes

--- assets/index.js
+++ assets/index.js
@@ -1,23 +1,5 @@
-const video = document.querySelector("video")
-const button = document.querySelector("button")
-
-function MediaPlayer(config){
-  this.media = config.el;
-}
-
-MediaPlayer.prototype.play = function (){
-  this.media.play();
-}
-
-MediaPlayer.prototype.pause = function (){
-  this.media.pause();
-}
-
+import MediaPlayer from './mediaPlayer.js';
+const video = document.querySelector("video");
 const player = new MediaPlayer({el:video});
-
-video.muted = true;
-button.onclick = () => {
-  (video.paused)
-  ? player.play()
-  : player.pause()
-}
+const button = document.querySelector("button");
+button.onclick = () => player.togglePlay();
