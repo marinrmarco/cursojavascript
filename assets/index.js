d3807d9c1e117502957cb61dc280f09ede348feb
marinrmarco <marinrosalesmarco@gmail.com>
2021-05-22 18:48:02 -0300

    Clousures and This

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,12 @@
 import MediaPlayer from './mediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
 const video = document.querySelector("video");
-const player = new MediaPlayer({el:video});
-const button = document.querySelector("button");
-button.onclick = () => player.togglePlay();
+const player = new MediaPlayer({
+  el:video, 
+  plugins: [
+    //new AutoPlay()
+]});
+const btnPlayPause = document.getElementById("play_pause");
+const btnMuteUnmute = document.getElementById("mute_unmute");
+btnPlayPause.onclick = () => player.togglePlay();
+btnMuteUnmute.onclick = () => player.toggleMute();
--- assets/mediaPlayer.js
+++ assets/mediaPlayer.js
@@ -1,6 +1,14 @@
 function MediaPlayer(config){
     this.media = config.el;
+    this.plugins = config.plugins || [];
+    this._initPlugins();
   };
+
+  MediaPlayer.prototype._initPlugins = function() {
+    this.plugins.forEach(plugin => {
+      plugin.run(this);
+    })
+  }
   
   MediaPlayer.prototype.play = function (){
     this.media.muted = true;
@@ -16,5 +24,19 @@ function MediaPlayer(config){
     ? this.play()
     : this.pause();
   };
+
+  MediaPlayer.prototype.mute = function(){
+    this.media.muted = true;
+  }
+
+  MediaPlayer.prototype.unmute = function(){
+    this.media.muted = false;
+  }
+
+  MediaPlayer.prototype.toggleMute = function(){
+    (this.media.muted)
+    ? this.unmute()
+    : this.mute();
+  }
 
   export default MediaPlayer;
